099ee3be194da1a2e1132a05231dea03af2e5e53
k <handykameron@live.com>
2025-07-04 14:36:07 -0800

    completed math page moving on to other tables

--- client/src/Components/Registration.jsx
+++ client/src/Components/Registration.jsx
@@ -1,5 +1,4 @@
 import {useState} from 'react'
-import Table from 'react-bootstrap/Table';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
--- client/src/Pages/Math.jsx
+++ client/src/Pages/Math.jsx
@@ -1,16 +1,26 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import axios from "axios"
 
-export default function User_login() {
+export default function Math() {
 
-  const[userQ, setuserQ]=useState({
-    Q:"",
-    A:"",
-  })
+  const[userQ, setuserQ]=useState([
+    {Q:"", A:""}
+  ])
 
+  useEffect(() =>{
+    const fetchResponse = async () => {
+      console.log("a")
+      console.log ("Use Effect ...")
+      const response = await axios.get("http://localhost:4000/math/")
+      console.log(response)
+      console.log(response.data)
+      setuserQ (response.data)
+    }
+    fetchResponse();
+   }, [])
 
   function UserInput (event){
     const{name,value}= (event.target)
@@ -27,18 +37,9 @@ export default function User_login() {
     }
   }
 
-
-
-
-
-
-
-
   return (
     <>
-
-
-<Table striped bordered hover>
+    <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
@@ -47,18 +48,20 @@ export default function User_login() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
+        {userQ.map((user,index)=>(
+          <tr key={index}>
+            <td>{index+1}</td>
+            <td>{user.Q}</td>
+            <td>{user.A}</td>
+          </tr>
+        ))}
       </tbody>
     </Table>
 
     <Form>
     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Question</Form.Label>
-        <Form.Control as="textarea" type="email" placeholder="name@example.com" name="Q" onChange={(event) => UserInput(event)}/>
+        <Form.Control as="textarea" type="email" placeholder="Math Question" name="Q" onChange={(event) => UserInput(event)}/>
     </Form.Group>
     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Answer</Form.Label>
